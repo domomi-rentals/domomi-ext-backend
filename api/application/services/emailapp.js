5bd9775ba0adf915180673ae50dab84c29528401
DESKTOP-QFC07BF\Kushal K <kkundanmal@domomi.com>
2017-06-21 19:31:23 -0700

    Created PDF generation service.

--- api/application/services/emailapp.js
+++ api/application/services/emailapp.js
@@ -50,14 +50,7 @@ module.exports = {
 
         console.log(mailOptions.attachments);
 
-        // send mail with defined transport object
-        // transporter.sendMail(mailOptions, (error, info) => {
-        //     if (error) {
-        //         return console.log(error);
-        //     }
-        //     console.log('Message %s sent: %s', info.messageId, info.response);
-        //     return mailOptions;
-        // });
+        // TODO: ADD emails to database.
         return transporter.sendMail(mailOptions);
 
     }
---
+++ api/renter/services/ticket.js
@@ -0,0 +1,114 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+
+// Public node modules.
+
+//var pdfFiller = require('node-pdffiller');
+var fs = require('fs');
+var PDFDocument = require('pdfkit');
+// var pdfkitForm = require('pdfkit-form');
+var pdfMake = require('pdfmake');
+
+
+/**
+ * Renter ticket service.
+ */
+
+module.exports = {
+
+    createTickee: function(){
+        var pdf = new PDFDocument({
+            size: 'LEGAL', // See other page sizes here: https://github.com/devongovett/pdfkit/blob/d95b826475dd325fb29ef007a9c1bf7a527e9808/lib/page.coffee#L69
+            info: {
+                Title: 'DomoMi',
+                Author: 'Some Author',
+            }
+        });
+
+        // Write stuff into PDF
+        pdf.text('Hello World');
+
+        // Stream contents to a file
+        pdf.pipe(
+            fs.createWriteStream('data/renter-tickets/test_complete.pdf')
+        )
+            .on('finish', function () {
+                console.log('PDF closed');
+            });
+
+        // Close PDF and write file.
+        pdf.end();
+
+        return pdf;
+
+    },
+
+    createTicket: function (renterInfo) {
+
+
+        try {
+            var fonts = {
+                Roboto: {
+                    normal: 'data/fonts/Roboto-Regular.ttf',
+                    bold: 'data/fonts/Roboto-Medium.ttf',
+                    italics: 'data/fonts/Roboto-Italic.ttf',
+                    bolditalics: 'data/fonts/Roboto-MediumItalic.ttf'
+	            }
+            };
+
+            let printer = new pdfMake(fonts);
+            let docDefinition = {
+                content: [
+                    {text:'DomoMi Renter Ticket',style:'header'},
+                    {text:'The Solution to Renter Verification.',style:'subheader'},
+                    
+                    {   style: 'tableStyle',
+                        layout: 'noBorders',
+                        table: {
+                            widths: [150, 200],
+                            body: [
+                                ['First Name', renterInfo.fname],
+                                ['Last Name', renterInfo.lname],
+                                ['Birthdate', renterInfo.birthdate],
+                                ['Phone Number', renterInfo.phone]
+                            ]
+                        }
+                    }
+                ],
+
+                styles: {
+                    header: {
+                        fontSize: 22,
+                        bold: true,
+                        alignment:'center'
+                    },
+                    subheader: {
+                        alignment:'center',
+                        margin: [0, 0, 0, 40]
+                    },
+                    tableStyle: {
+                        
+                    }
+                }
+            };
+            let path = "data/renter-tickets/";
+            let name = renterInfo.fname+"-"+renterInfo.id;
+            let pdfDoc = printer.createPdfKitDocument(docDefinition);
+            pdfDoc.pipe(fs.createWriteStream(path+name+'.pdf'));
+            pdfDoc.end();
+            console.log("DONE!");
+
+        } catch (err) {
+            console.log("Error: ", err);
+        }
+       
+
+    }
+
+    
+
+
+};
